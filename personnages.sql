c90d8b349156f66d5e7270d4852d3abd75f43d70
Giroux julien <girojul@ndlp.fr>
2020-09-17 08:37:07 +0200

    ajout de la base de donnée

---
+++ personnages.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 17 sep. 2020 à 06:36
+-- Version du serveur :  10.5.5-MariaDB
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `battlegame`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personnages`
+--
+
+DROP TABLE IF EXISTS `personnages`;
+CREATE TABLE IF NOT EXISTS `personnages` (
+  `nom` varchar(50) COLLATE utf8_bin NOT NULL,
+  `force` int(11) NOT NULL,
+  `pv` int(11) NOT NULL,
+  `niveau` int(11) NOT NULL,
+  `experience` int(11) NOT NULL,
+  `type` varchar(50) COLLATE utf8_bin NOT NULL,
+  `atout` int(11) NOT NULL,
+  `reveil` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `personnages`
+--
+
+INSERT INTO `personnages` (`nom`, `force`, `pv`, `niveau`, `experience`, `type`, `atout`, `reveil`, `id`) VALUES
+('juju', 1675, 21250, 1, 0, 'Magicien', 0, 0, 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
